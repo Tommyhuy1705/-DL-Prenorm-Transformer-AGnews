70d58f1181ae509ea02c27378de2d35a2623a7c7
Dong <duongquangdong1008@gml.com>
2026-05-18 10:01:59 +0700

    add model and train.py

---
+++ model/__init__.py
@@ -0,0 +1,10 @@
+"""Gói models cho các kiến trúc Transformer.
+
+Chứa các cài đặt attention và các biến thể Transformer.
+"""
+
+__all__ = [
+    "attention",
+    "transformer_postnorm",
+    "transformer_prenorm",
+]
